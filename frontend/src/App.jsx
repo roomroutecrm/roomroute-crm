94ec1652585f085787852895e89a1bee6441bfb8
roomroutecrm <josh.gaddis@roomroute.org>
2025-11-07 17:18:31 -0600

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to RoomRoute CRM</h1>
+      <p>The frontend is now connected and ready!</p>
+    </div>
+  );
+}
+
+export default App;
